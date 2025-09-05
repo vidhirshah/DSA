a632e26e3c3003b25782a86a1c05e69d81c89c21
vidhirshah <vidhirshah24@gmail.com>
2025-09-05 05:46:06 +0530

    recursion

--- dp/3/maxChoco2.py
+++ dp/3/maxChoco2.py
@@ -1,38 +1,34 @@
 def maxChocolates(g):
     dp = []
     length = len(g)
+    INT_MIN = -10**4
     for i in range(length):
         dp.append([])
         for j in range(length):
             dp[i].append([])
             for k in range(length):
                 dp[i][j].append(-1)
-    def helper(m,n1,n2):
-        length = len(g)
-        INT_MIN = -10**4
-        if m >= length or n1 >= length or n2 >= length:
-            return INT_MIN
-        if n1 < 0 or n2 < 0:
-            return INT_MIN
-        if m == length - 1:
-            if n1 == n2:
-                ans = g[m][n1]
-            else:
-                ans = g[m][n1] + g[m][n2]
-            dp[m][n1][n2] = ans
-        if dp[m][n1][n2] != -1:
-            return dp[m][n1][n2]
-        maxi = INT_MIN
-        for d1 in range(-1,2):
-            for d2 in range(-1,2):
-                maxi = max(helper(m+1,n1+d1,n2+d2),maxi)
-        if n1 == n2:
-            ans =  maxi + g[m][n1]
-        else:
-            ans =  maxi + g[m][n1]+g[m][n2]
-        dp[m][n1][n2] = ans
-        return ans
-    return helper(0,0,len(g)-1)
+    dp[0][0][length-1] = g[0][0]   + g[0][length-1]
+    for m in range(1,length):
+        for n1 in range(length):
+            for n2 in range(length):
+                maxi = INT_MIN
+                if m == 1  :
+                    for d1 in range(-1,2):
+                        for d2 in range(-1,2):
+                            if n1 + d1 == 0 and n2 + d2 == length -1:
+                                maxi = max(dp[m-1][n1+d1][n2+d2],maxi)
+                if m > 1:
+                    for d1 in range(-1,2):
+                        for d2 in range(-1,2):
+                            if n1 + d1 >= 0 and n1 + d1 <= length -1 and n2 + d2 >= 0 and n2 + d2 <= length -1:
+                                maxi = max(dp[m-1][n1+d1][n2+d2],maxi)
+                if n1 == n2:
+                    ans =  maxi + g[m][n1]
+                else:
+                    ans =  maxi + g[m][n1]+g[m][n2]
+                dp[m][n1][n2] = ans
+    return max(dp[length-1][c1][c2] for c1 in range(length) for c2 in range(length) if dp[length-1][c1][c2] != -1)
 
 i1 =  [[2, 3, 1, 2],[3, 4, 2, 2],[5, 6, 3, 5]]
 i2 = [[1, 2],[3, 4]]
